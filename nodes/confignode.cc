715d8429d30e8cb2e45adc96aedb0869ac188c62
Chris Van Arsdale <chris.vana@gmail.com>
2013-07-29 15:18:33 -0700

    components

---
+++ nodes/confignode.cc
@@ -0,0 +1,55 @@
+// Copyright 2013
+// Author: Christopher Van Arsdale
+
+#include <string>
+#include <vector>
+#include "common/log/log.h"
+#include "common/strings/path.h"
+#include "env/input.h"
+#include "nodes/confignode.h"
+#include "reader/buildfile.h"
+
+using std::string;
+using std::vector;
+
+namespace repobuild {
+
+void ConfigNode::Parse(BuildFile* file, const BuildFileNode& input) {
+  Node::Parse(file, input);
+  if (ParseStringField(input, "component_src", &component_src_root_)) {
+    file->AddBaseDependency(target().full_path());
+  }
+}
+
+void ConfigNode::WriteMakefile(const Input& input,
+                               const vector<const Node*>& all_deps,
+                               string* out) const {
+  if (!component_src_root_.empty() && component_src_root_ != ".") {
+    // This is the directory we create.
+    string dir = strings::JoinPath(input.source_dir(), component_src_root_);
+    out->append(dir);
+    out->append(":\n");
+
+    // ... And this is how we create it:
+    // mkdir -p src; ln -s path/to/this/target/dir src/<component>
+    out->append("\t");
+    out->append("mkdir -p ");
+    out->append(dir);
+    out->append("; ln -s ");
+    out->append(target().dir());
+    out->append(" ");
+    out->append(dir);
+    out->append("\n\n");
+  }
+}
+
+void ConfigNode::DependencyFiles(const Input& input,
+                                 vector<string>* files) const {
+  Node::DependencyFiles(input, files);
+  if (!component_src_root_.empty() && component_src_root_ != ".") {
+    string dir = strings::JoinPath(input.source_dir(), component_src_root_);
+    files->push_back(dir);
+  }
+}
+
+}  // namespace repobuild
---
+++ nodes/confignode.h
@@ -0,0 +1,29 @@
+// Copyright 2013
+// Author: Christopher Van Arsdale
+
+#ifndef _REPOBUILD_NODES_CONFIGNODE_H__
+#define _REPOBUILD_NODES_CONFIGNODE_H__
+
+#include <string>
+#include "nodes/node.h"
+
+namespace repobuild {
+
+class ConfigNode : public Node {
+ public:
+  ConfigNode(const TargetInfo& t) : Node(t) {}
+  virtual ~ConfigNode() {}
+  virtual std::string Name() const { return "config"; }
+  virtual void Parse(BuildFile* file, const BuildFileNode& input);
+  virtual void WriteMakefile(const Input& input,
+                             const std::vector<const Node*>& all_deps,
+                             std::string* out) const;
+  virtual void DependencyFiles(const Input& input,
+                               std::vector<std::string>* files) const;
+ protected:
+  std::string component_src_root_;
+};
+
+}  // namespace repobuild
+
+# endif  // _REPOBUILD_NODES_CONFIGNODE_H__
