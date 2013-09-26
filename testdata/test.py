ea63564ac6ffe5929be33eabfd13c1078ce6eeee
Chris Van Arsdale <chris.vana@gmail.com>
2013-09-26 10:44:31 -0700

    .

--- testdata/test.py
+++ testdata/test.py
@@ -1,9 +1,14 @@
 import a.test
+import common.base.flags as flags
 import testdata.a.a_pb2
+import sys
 
-def main():
+flags.d.DEFINE_string('pass_text', 'PASS', 'What to print')
+
+def main(argv):
+    flags.Parse(argv)
     testdata.a.a_pb2.FooProto()
-    print("PASS")
+    print(flags.FLAGS.pass_text)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
